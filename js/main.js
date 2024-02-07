032dc5c221ced7dd715e3265cbbde8174aa9d98d
Edgar Herrera <edgarh09lfc@gmail.com>
2024-02-06 23:45:35 -0800

    Add icons to menu and change color

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const navbar = document.querySelector('.navbar');
 
+
 menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('is-active');
-    navbar.classList.toggle('is-active')
+    navbar.classList.toggle('is-active');
+    
 })
